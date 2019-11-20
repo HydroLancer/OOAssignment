3722f7ab9371238698adb57c524efa56db95f1b6
HydroLancer <36414472+HydroLancer@users.noreply.github.com>
2019-11-20 01:30:53 +0000

    working with basic datatypes
    int, char tested and fine so far
    assumed float and double just fine too.
    
    added a bunch of methods

--- OOMapTemplate/MapTemplate.h
+++ OOMapTemplate/MapTemplate.h
@@ -10,34 +10,129 @@ template<class T, class U> class MapTemplate
 	};
 
 private:
-	keyPair pairs[1000];
-	
-
-
+	keyPair* pairs;
+	int ARR_SIZE;
 public:
-	MapTemplate()
+	//pass in the size of the array, otherwise defaults to size 1000
+	MapTemplate(int size = 1000) 
 	{
-		for (int i = 0; i < 1000; ++i)
-		{
-			pairs[i] = keyPair{ 0 , 0 };
+		ARR_SIZE = size;
+		pairs = new keyPair[size];
 
+		//Fills the array with 0, so when scannng through to find an "empty" slot, first 0 in the key is fine to overwrite
+		//need to work out how to sort this for string.. 
+		for (int i = 0; i < size; i++)
+		{
+			pairs[i] = { 0,0 };
+			
+		}
+		std::cout << "Made It Out" << std::endl;
+	}
+
+	//cycles through the array until it finds the first 'empty' slot (i.e, key being 0) and puts it there.
+	bool push(U key, T data)
+	{
+		//though it will reject a key if it's integer 0.
+		if (key == 0)
+		{
+			std::cout << "Rejected" << std::endl;
+			return false;
+		}
+		else
+		{
+			for (int i = 0; i < ARR_SIZE; i++)
+			{
+				if (pairs[i].key != 0)
+				{
+					//just continues
+				}
+				else
+				{
+					pairs[i].key = key;
+					pairs[i].data = data;
+					break;
+				}
+			}
+			return true;
+		}
+
+	}
+
+	//deletes a specific member by replacing the key and data with 0.
+	void erase(U key)
+	{
+		for (int i = 0; i < ARR_SIZE; i++)
+		{
+			if (pairs[i].key == key)
+			{
+				pairs[i].key = 0;
+				pairs[i].data = 0;
+				std::cout << "Data Erased" << std::endl;
+				return;
+			}
+		}
+		std::cout << "key not found" << std::endl;
+		return;
+	}
+
+	//returns true if array is empty.
+	bool empty()
+	{
+		int counter = 0;
+		for (int i = 0; i < ARR_SIZE; i++)
+		{
+			if (pairs[i].key != 0)
+			{
+				counter++;
+			}
+		}
+		if (counter == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
 		}
 	}
 
-	void push(T data, U key)
+	//checks to see how many items are in the array
+	int size()
 	{
-		
+		int counter = 0;
+		for (int i = 0; i < ARR_SIZE; i++)
+		{
+			if (pairs[i].key != 0)
+			{
+				counter++;
+			}
+		}
+		return counter;
 	}
 
-	void pop()
-	{
-
-	}
-
+	//outputs the data attached to a key.
 	U output(U key)
 	{
+		for (int i = 0; i < ARR_SIZE; i++)
+		{
+			if (pairs[i].key == key)
+			{
+				return pairs[i].data;
+			}
+			else if (pairs[i].key == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				//nowt
+			}
+		}
 
-		return pairs[index].data;
 	}
 
+	~MapTemplate()
+	{
+		delete pairs;
+	}
 };
--- OOMapTemplate/OOMapTemplate.cpp
+++ OOMapTemplate/OOMapTemplate.cpp
@@ -2,10 +2,29 @@
 //
 #include "MapTemplate.h"
 #include <iostream>
+#include <string>
 
 int main()
 {
-	MapTemplate<int, int>* test = new MapTemplate<int, int>;
-	test->push();
+	MapTemplate<int, int>* test = new MapTemplate<int, int>(50);
+	test->push(9, 5);
+	test->push(4, 2);
+	test->push(1, 6);
+	std::cout << test->output(9) << std::endl;
+	std::cout << test->output(4) << std::endl;
+	std::cout << test->output(1) << std::endl;
+	test->erase(11);
+	std::cout << "Array size: " << test->size() << std::endl;
+	std::cout << "---------------" << std::endl;
+
+	MapTemplate<char, char>* test2 = new MapTemplate<char, char>;
+	test2->push('a', '4');
+	test2->push('b', '6');
+	std::cout << test2->output('b') << std::endl;
+	std::cout << test2->output('a') << std::endl;
 	system("pause");
+
+	delete test;
+	delete test2;
+	return 0;
 }
